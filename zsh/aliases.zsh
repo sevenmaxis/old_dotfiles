03d1f0554a00aa42901a22d07c94935180a39bc8
Sergey Gopkalo <sevenmaxis@gmail.com>
2015-12-11 15:02:50 +0200

    alias for pass

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -211,3 +211,7 @@ alias dbmu='spring rake db:migrate:up'
 
 # Homebrew
 alias brewu='brew update  && brew upgrade --all && brew cleanup && brew prune && brew doctor'
+
+# Personal
+alias mkcd="mkdcd"
+alias p="pass -c"

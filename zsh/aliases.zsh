7c01b06ebb5b93a99a84f18721d7b20ee5561436
Sergey Gopkalo <sevenmaxis@gmail.com>
2016-09-30 19:46:13 +0300

    rails helper

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -246,3 +246,27 @@ alias blueon="blueutil power 1"
 alias blueoff="blueutil power 0"
 
 alias pb="tr -d '\n' | pbcopy"
+
+function rails() {
+  if [ "$1" = "start" ]; then
+     if [ "$2" = "" ]; then
+        RENV="development"
+     else
+        RENV="$2"
+     fi
+     rails server -d -e "$RENV"
+     return 0
+  elif [ "$1" = "stop" ]; then
+     if [ -f tmp/pids/server.pid ]; then
+        kill $2 $(cat tmp/pids/server.pid)
+        return 0
+     else
+        echo "It seems there is no server running or you are not in a rails project root directory"
+        return 1
+     fi
+  elif [ "$1" = "restart" ]; then
+     rails stop && rails start $2
+  else
+     command rails $@
+  fi;
+}

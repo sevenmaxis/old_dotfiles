875717cc8196ad2ae96407e51c0f1f1981efcf9f
Sergey Gopkalo <sevenmaxis@gmail.com>
2017-01-13 21:54:02 +0200

    alias for mktouch

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -273,3 +273,15 @@ function rails() {
      command rails $@
   fi;
 }
+
+mktouch() {
+  if [ $# -lt 1 ]; then
+    echo "Missing argument";
+    return 1;
+  fi
+
+  for f in "$@"; do
+    mkdir -p -- "$(dirname -- "$f")"
+    touch -- "$f"
+  done
+}

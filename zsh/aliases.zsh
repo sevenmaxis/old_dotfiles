eaafccbe48dbd569b8be4a567051f919fb6c2a75
Sergey Gopkalo <sevenmaxis@gmail.com>
2017-12-30 17:42:14 +0200

    add cask cleanup to brewu

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -215,7 +215,9 @@ alias dbmd='spring rake db:migrate:down'
 alias dbmu='spring rake db:migrate:up'
 
 # Homebrew
-alias brewu='brew update && brew upgrade && brew cleanup && brew prune && brew doctor'
+alias brewu='brew update && brew upgrade &&\
+             brew cleanup && brew cask cleanup &&\
+             brew prune && brew doctor'
 
 # Personal
  alias p="pass -c"
